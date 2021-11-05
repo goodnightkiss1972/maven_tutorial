b6b977917768db8c192fdd5adc7193de43d0bb07
goodnightkiss1972 <goodnightkiss1972@gmail.com>
2021-11-05 15:17:50 +0100

    Ajout de assertj aux dependances

---
+++ sample-project/src/test/java/goodnightkiss/training/App2Test.java
@@ -0,0 +1,22 @@
+package goodnightkiss.training;
+
+//import static org.junit.Assert.assertTrue;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class App2Test 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertThat(true).isEqualTo(true);
+    }
+}
