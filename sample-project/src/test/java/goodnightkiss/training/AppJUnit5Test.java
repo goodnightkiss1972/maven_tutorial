62f9dc016f79f745574d5accf68c15a9ad3cfa7c
goodnightkiss1972 <goodnightkiss1972@gmail.com>
2021-11-05 17:41:42 +0100

    Ajout de la dependance Junit 5

--- sample-project/src/test/java/goodnightkiss/training/AppJUnit5Test.java
+++ sample-project/src/test/java/goodnightkiss/training/AppJUnit5Test.java
@@ -1,20 +1,20 @@
 package goodnightkiss.training;
 
-//import static org.junit.Assert.assertTrue;
-
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 
 /**
  * Unit test for simple App.
  */
-public class App2Test 
+public class AppJUnit5Test 
 {
     /**
      * Rigorous Test :-)
      */
     @Test
+    @DisplayName("Mon test simple en JUnit 5")
     public void shouldAnswerWithTrue()
     {
         assertThat(true).isEqualTo(true);
