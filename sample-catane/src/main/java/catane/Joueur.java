4b747244441ba09164327631cd955b9597dfef71
goodnightkiss1972 <goodnightkiss1972@gmail.com>
2021-11-11 16:16:51 +0100

    ajout du type de joueur

--- sample-catane/src/main/java/catane/Joueur.java
+++ sample-catane/src/main/java/catane/Joueur.java
@@ -1,16 +1,19 @@
 package catane;
 
 import enums.Couleur;
+import enums.TypeJoueur;
 
 public class Joueur {
     String nom;
     Couleur couleur;
+    TypeJoueur typeJoueur;
 
-    public Joueur(String nom, Couleur couleur) {
-        if  (nom == null || couleur == null) {
-            throw new IllegalArgumentException("Le joueur ne peut pas etre cree sans nom ou sans couleur !!");
+    public Joueur(String nom, Couleur couleur, TypeJoueur typeJoueur) {
+        if  (nom == null || couleur == null || typeJoueur == null) {
+            throw new IllegalArgumentException("Le joueur ne peut pas etre cree sans nom ou sans couleur ou sans type !!");
         }
         this.nom = nom;
         this.couleur = couleur;
+        this.typeJoueur = typeJoueur;
     }
 }
--- sample-catane/src/test/java/catane/JoueurUTest.java
+++ sample-catane/src/test/java/catane/JoueurUTest.java
@@ -3,6 +3,7 @@ package catane;
 import org.junit.jupiter.api.Test;
 
 import enums.Couleur;
+import enums.TypeJoueur;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,24 +12,30 @@ public class JoueurUTest {
 
     @Test
     public void creerJoueurSansRien() {
-        assertThatThrownBy(() -> new Joueur(null, null)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Joueur(null, null, null)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void creerJoueurSansNom() {
-        assertThatThrownBy(() -> new Joueur(null, Couleur.BLEU)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Joueur(null, Couleur.BLEU, TypeJoueur.IA)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void creerJoueurSansCouleur() {
-        assertThatThrownBy(() -> new Joueur("Fabrice", null)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Joueur("Fabrice", null, TypeJoueur.HUMAIN)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void creerJoueurSansType() {
+        assertThatThrownBy(() -> new Joueur("Fabrice", Couleur.BLEU, null)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void creerJoueur() {
-        Joueur fabrice = new Joueur("fabrice", Couleur.BLEU);
+        Joueur fabrice = new Joueur("fabrice", Couleur.BLEU, TypeJoueur.HUMAIN);
         assertThat(fabrice.nom).isEqualTo("fabrice");
         assertThat(fabrice.couleur).isEqualTo(Couleur.BLEU);
+        assertThat(fabrice.typeJoueur).isEqualTo(TypeJoueur.HUMAIN);
     }
 
 
