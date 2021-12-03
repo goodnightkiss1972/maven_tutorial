b224f24f0da67c50269739c4c43a3feb857fc2bb
goodnightkiss1972 <goodnightkiss1972@gmail.com>
2021-12-03 16:51:52 +0100

    debut du jeu en lui meme

---
+++ sample-catane/src/main/java/catane/Catane.java
@@ -0,0 +1,26 @@
+package catane;
+
+import enums.Couleur;
+import enums.TypeJoueur;
+import utils.De6Faces;
+import java.util.List;
+import java.util.OptionalLong;
+import java.util.ArrayList;
+
+public class Catane {
+    
+    public static void main(String[] args) {
+        // point de depart du jeu
+        De6Faces d1 = new De6Faces();
+        De6Faces d2 = new De6Faces();
+        Joueur j1 = new Joueur("eric", Couleur.BLEU, TypeJoueur.HUMAIN);
+        Joueur j2 = new Joueur("fabien", Couleur.BLANC, TypeJoueur.IA);
+        Joueur j3 = new Joueur("jade", Couleur.ROUGE, TypeJoueur.HUMAIN);
+        List<Joueur> joueurs = new ArrayList<Joueur>();
+        joueurs.add(j1);
+        joueurs.add(j2);
+        joueurs.add(j3);
+        Jeu jeu = new Jeu(d1, d2, joueurs, 5, 3, OptionalLong.of(1234));
+    }
+
+}
