73716aa0107e42c91f551c7905972f23e3c41394
goodnightkiss1972 <goodnightkiss1972@gmail.com>
2021-12-03 14:06:18 +0100

    certification plateau en pseudo aleatoire force (seed)

--- sample-catane/src/test/java/catane/Plateau05JetonsUTest.java
+++ sample-catane/src/test/java/catane/Plateau05JetonsUTest.java
@@ -11,6 +11,7 @@ public class Plateau05JetonsUTest {
     public void creerPlateau5x3AvecGraine() {
         Plateau plateau = new Plateau(5, 3, OptionalLong.of(1234));
         plateau.dessinePlateau();
+        assertThat(plateau.getTuiles().get(2).getJeton()).isEqualTo(8);
     }
 
 }
