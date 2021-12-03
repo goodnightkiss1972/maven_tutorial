26281557a727524b8fdeadb4649df3b2613ab310
goodnightkiss1972 <goodnightkiss1972@gmail.com>
2021-12-03 16:20:06 +0100

    controles sur la liste des joueurs

--- sample-catane/src/main/java/catane/Jeu.java
+++ sample-catane/src/main/java/catane/Jeu.java
@@ -13,6 +13,12 @@ public class Jeu {
     private Plateau plateau;
 
     public Jeu(De6Faces d1, De6Faces d2, List<Joueur> joueurs, Integer tailleHorizontale, Integer tailleVerticale, OptionalLong seedPlateau) {
+        if (d1 == null || d2 == null || joueurs == null) {
+            throw new IllegalArgumentException("il faut 2 des et 3 ou 4 joueurs !");
+        }
+        if (joueurs.size() < 3 || joueurs.size() > 4) {
+            throw new IllegalArgumentException("le jeu se joue a 3 ou 4 joueurs maximum");
+        }
         this.d1 = d1;
         this.d2 = d2;
         this.joueurs = joueurs;
--- sample-catane/src/test/java/catane/JeuUTest.java
+++ sample-catane/src/test/java/catane/JeuUTest.java
@@ -9,8 +9,44 @@ import java.util.List;
 import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class JeuUTest {
+
+    @Test
+    public void creeJeuSansRien() {
+        assertThatThrownBy(() -> new Jeu(null, null, null, 1, 1, null)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void creeJeuAvecTropPeuDeJoueurs() {
+        De6Faces d1 = new De6Faces();
+        De6Faces d2 = new De6Faces();
+        Joueur j1 = new Joueur("j1", Couleur.BLEU, TypeJoueur.HUMAIN);
+        Joueur j2 = new Joueur("j2", Couleur.BLANC, TypeJoueur.HUMAIN);
+        List<Joueur> joueurs = new ArrayList<Joueur>();
+        joueurs.add(j1);
+        joueurs.add(j2);
+        assertThatThrownBy(() -> new Jeu(d1, d2, joueurs, 1, 1, null)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void creeJeuAvecTropDeJoueurs() {
+        De6Faces d1 = new De6Faces();
+        De6Faces d2 = new De6Faces();
+        Joueur j1 = new Joueur("j1", Couleur.BLEU, TypeJoueur.HUMAIN);
+        Joueur j2 = new Joueur("j2", Couleur.BLANC, TypeJoueur.HUMAIN);
+        Joueur j3 = new Joueur("j3", Couleur.BLANC, TypeJoueur.HUMAIN);
+        Joueur j4 = new Joueur("j4", Couleur.BLANC, TypeJoueur.HUMAIN);
+        Joueur j5 = new Joueur("j5", Couleur.BLANC, TypeJoueur.HUMAIN);
+        List<Joueur> joueurs = new ArrayList<Joueur>();
+        joueurs.add(j1);
+        joueurs.add(j2);
+        joueurs.add(j3);
+        joueurs.add(j4);
+        joueurs.add(j5);
+        assertThatThrownBy(() -> new Jeu(d1, d2, joueurs, 1, 1, null)).isInstanceOf(IllegalArgumentException.class);
+    }
 
     @Test
     public void TestLance100FoisDeuxDes() {
@@ -18,7 +54,11 @@ public class JeuUTest {
         De6Faces d2 = new De6Faces();
         List<Joueur> joueurs = new ArrayList<Joueur>();
         Joueur j1 = new Joueur("fabien", Couleur.BLEU, TypeJoueur.HUMAIN);
+        Joueur j2 = new Joueur("georges", Couleur.BLANC, TypeJoueur.HUMAIN);
+        Joueur j3 = new Joueur("emile", Couleur.ROUGE, TypeJoueur.HUMAIN);
         joueurs.add(j1);
+        joueurs.add(j2);
+        joueurs.add(j3);
         Jeu jeu = new Jeu(d1, d2, joueurs, 5, 3, null);
         Integer i = 0;
         while (i < 100) {
