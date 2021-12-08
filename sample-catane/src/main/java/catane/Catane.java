cdd9945e0f6d9d9071c347c0b3d8219dd98cb0d4
goodnightkiss1972 <goodnightkiss1972@gmail.com>
2021-12-08 08:55:45 +0100

    initialisation des parametres du jeu

--- sample-catane/src/main/java/catane/Catane.java
+++ sample-catane/src/main/java/catane/Catane.java
@@ -8,29 +8,45 @@ import utils.DialogueJ;
 
 import java.util.List;
 import java.util.OptionalLong;
-import java.io.Console;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Catane {
-    
+
+    private static ConsoleJ console = new ConsoleJ();
+    private static DialogueJ dialogue = new DialogueJ();
+
     public static void main(String[] args) {
         // point de depart du jeu
         De6Faces d1 = new De6Faces();
         De6Faces d2 = new De6Faces();
-        Joueur j1 = new Joueur("eric", Couleur.BLEU, TypeJoueur.HUMAIN);
-        Joueur j2 = new Joueur("fabien", Couleur.BLANC, TypeJoueur.IA);
-        Joueur j3 = new Joueur("jade", Couleur.ROUGE, TypeJoueur.HUMAIN);
-        List<Joueur> joueurs = new ArrayList<Joueur>();
-        joueurs.add(j1);
-        joueurs.add(j2);
-        joueurs.add(j3);
-        //Jeu jeu = new Jeu(d1, d2, joueurs, 5, 3, OptionalLong.of(1234));
-        ConsoleJ console = new ConsoleJ();
-        DialogueJ dialogue = new DialogueJ();
+        List<Joueur> listeJoueurs = new ArrayList<Joueur>();
         console.effaceTout();
-
-
+        console.println(Couleur.MAUVE.getStylo(), "*****************");
+        console.println(Couleur.JAUNE.getStylo(), "   C A T A N E   ");
+        console.println(Couleur.MAUVE.getStylo(), "*****************");
+        console.aLaLigne();
+        Integer tailleHorizontale = dialogue.demandeEntier(Couleur.BLEU.getStylo(), "Saisissez la taille horizontale du jeu : ");
+        Integer tailleVerticale = dialogue.demandeEntier(Couleur.BLEU.getStylo(), "Saisissez la taille verticale du jeu : ");
+        console.aLaLigne();
+        Integer nombreJoueurs = dialogue.demandeEntierPrecis(Couleur.BLEU.getStylo(), "Veuillez saisir le nombre de joueurs : ", Arrays.asList(3, 4));
+        for (Integer i = 1; i <= nombreJoueurs; i++) {
+            String nomJoueur = dialogue.demandeChaine(Couleur.BLEU.getStylo(), "Joueur " + i + " quel est votre prenom ? ");
+            Couleur couleurJoueur = Couleur.valueOf(dialogue.demandeChainePrecise(Couleur.BLEU.getStylo(), "Joueur " + i + " choisissez une couleur ", Arrays.asList("BLEU", "BLANC", "ROUGE", "VERT")));
+            TypeJoueur typeJoueur = TypeJoueur.valueOf(dialogue.demandeChainePrecise(Couleur.BLEU.getStylo(), "Joueur " + i + " etes vous humain ou ordinateur ? ", Arrays.asList("HUMAIN", "IA")));
+            Joueur joueur = new Joueur(nomJoueur, couleurJoueur, typeJoueur);
+            listeJoueurs.add(joueur);
+            console.aLaLigne();
+        }
+        Jeu jeu;
+        if (dialogue.demandeChainePrecise(Couleur.VERT.getStylo(), "Jeu aleatoire ? ", Arrays.asList("O", "N")).equals("N")) {
+            jeu = new Jeu(d1, d2, listeJoueurs, tailleHorizontale, tailleVerticale, OptionalLong.of(Long.valueOf(dialogue.demandeEntier(Couleur.VERT.getStylo(), "Entrez le chiffre de votre choix"))));
+            console.println(Couleur.VERT.getStylo(), "Jeu pseudo aleatoire avec graine.");
+        }
+        else {
+            jeu = new Jeu(d1, d2, listeJoueurs, tailleHorizontale, tailleVerticale, null);
+            console.println(Couleur.VERT.getStylo(), "Jeu aleatoire.");
+        }
+        jeu.lanceJeu();
     }
-
 }
