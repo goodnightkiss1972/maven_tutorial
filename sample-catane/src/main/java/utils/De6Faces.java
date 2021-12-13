f639af3ea80ba747ee545e366aaa357bf9ef4bd7
goodnightkiss1972 <goodnightkiss1972@gmail.com>
2021-12-13 19:25:13 +0100

    correction du de qui sortait toujours la meme face

--- sample-catane/src/main/java/utils/De6Faces.java
+++ sample-catane/src/main/java/utils/De6Faces.java
@@ -11,21 +11,15 @@ public class De6Faces {
     public De6Faces() {
         this.nbrFaces = 6;
         rnd = new Random();
-        this.setTirage();
     }
 
     public De6Faces(long seed) {
         this.nbrFaces = 6;
         rnd = new Random(seed);
-        this.setTirage();
     }
 
     public Integer getTirage() {
-        return this.tirage;
-    }
-
-    public void setTirage() {
-        this.tirage = 1 + rnd.nextInt(nbrFaces);
+        return 1 + rnd.nextInt(nbrFaces);
     }
 
 }
--- sample-catane/src/test/java/utils/De6FacesNormalUTest.java
+++ sample-catane/src/test/java/utils/De6FacesNormalUTest.java
@@ -9,18 +9,9 @@ public class De6FacesNormalUTest {
     @Test
     void LanceDe6Faces() {
         De6Faces de6 = new De6Faces(1);
-        de6.setTirage();
-        assertThat(de6.getTirage()).isEqualTo(5);
-        de6.setTirage();
-        assertThat(de6.getTirage()).isEqualTo(2);
-        de6.setTirage();
         assertThat(de6.getTirage()).isEqualTo(4);
-        de6.setTirage();
-        assertThat(de6.getTirage()).isEqualTo(3);
-        de6.setTirage();
         assertThat(de6.getTirage()).isEqualTo(5);
-        de6.setTirage();
-        assertThat(de6.getTirage()).isEqualTo(3);
+        assertThat(de6.getTirage()).isEqualTo(2);
     }
 
     @Test
@@ -28,7 +19,6 @@ public class De6FacesNormalUTest {
         De6Faces de6 = new De6Faces();
         Integer i = 0;
         while (i < 6000) {
-            de6.setTirage();
             assertThat(de6.getTirage()).isBetween(1, 6);
             i++;
         }
